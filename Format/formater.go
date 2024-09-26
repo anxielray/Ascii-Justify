e822fe7442021d2f1693598a94d618a772030f2c
anxielray <anxielworld@gmil.com>
2024-09-26 22:42:35 +0300

    [Add]: Add the format file that contains the formatting functionalities

---
+++ Format/formater.go
@@ -0,0 +1,109 @@
+package Format
+
+import (
+	ascii "Terminal-Art/Ascii"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func FormatLeft(text string, width int) string {
+	return text
+}
+
+func FormatRight(text string, width int) string {
+	var result strings.Builder
+	lines := strings.Split(text, "\n")
+	for _, line := range lines {
+		result.WriteString(fmt.Sprintf("%*s\n", width, line))
+	}
+	return result.String()
+}
+
+func FormatCenter(text string, width int) string {
+	var result strings.Builder
+	lines := strings.Split(text, "\n")
+	for _, line := range lines {
+		padding := (width - len(line)) / 2
+		result.WriteString(fmt.Sprintf("%*s%*s\n", padding+len(line), line, padding, ""))
+	}
+	return result.String()
+}
+
+func FormatJustify(input, banner string, width int) string {
+	bannerFile := banner
+	bannerText, err := os.ReadFile(bannerFile)
+	if err != nil {
+		fmt.Println("Error reading from file:", err)
+		os.Exit(0)
+	}
+	fileInfo, err := os.Stat(bannerFile)
+	if err != nil {
+		fmt.Println("Error reading file information", err)
+		os.Exit(0)
+	}
+	fileSize := fileInfo.Size()
+	var contents []string
+	if fileSize == 6623 || fileSize == 4703 || fileSize == 7463 || fileSize == 4496 {
+		contents = strings.Split(string(bannerText), "\n")
+	} else {
+		return "Error: Invalid banner file size\n"
+	}
+
+	var result strings.Builder
+	lines := strings.Split(input, "\n")
+
+	for _, line := range lines {
+		words := strings.Fields(line)
+		if len(words) == 0 {
+			result.WriteString("\n")
+			continue
+		}
+		if len(words) == 1 {
+			asciiWord := ascii.AsciiArt([]string{words[0]}, contents)
+			result.WriteString(asciiWord)
+			continue
+		}
+
+		asciiWords := make([]string, len(words))
+		totalLength := 0
+		for i, word := range words {
+			asciiWords[i] = ascii.AsciiArt([]string{word}, contents)
+			totalLength += len(strings.Split(asciiWords[i], "\n")[0])
+		}
+
+		spaceCount := len(words) - 1
+		totalSpaces := width - totalLength
+		spaces := make([]int, spaceCount)
+		for i := range spaces {
+			spaces[i] = totalSpaces / spaceCount
+		}
+		for i := 0; i < totalSpaces%spaceCount; i++ {
+			spaces[i]++
+		}
+
+		asciiLines := make([][]string, 8)
+		for i := range asciiLines {
+			asciiLines[i] = make([]string, len(words))
+		}
+
+		for i, asciiWord := range asciiWords {
+			lines := strings.Split(asciiWord, "\n")
+			for j := 0; j < 8; j++ {
+				asciiLines[j][i] = lines[j]
+			}
+		}
+
+		for j := 0; j < 8; j++ {
+			for i, word := range asciiLines[j] {
+				result.WriteString(word)
+				if i < len(asciiLines[j])-1 {
+					result.WriteString(strings.Repeat(" ", spaces[i]))
+				}
+			}
+			result.WriteString("\n")
+		}
+	}
+
+	return result.String()
+}
